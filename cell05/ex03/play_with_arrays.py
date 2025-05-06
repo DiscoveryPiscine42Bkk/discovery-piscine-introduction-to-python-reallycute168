6c6e9015ea6885a7d7424cd0d9efc7321587c3e1
reallycute168 <reallycute49168@gmail.com>
2025-05-06 23:50:29 +0700

    gogo

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 num =  [2, 8, 9, 48, 8, 22, -12, 2]
+print(num)
 newnum = []
 for x in num:
     '''append(data)'''
-    result = x + 2
-    if result >= 5 :
-        newnum.append(result)
-print(num)
-print(newnum)
+    if x >= 5 :
+        newnum.append(x + 2)
+'''set ลบค่าซ้ำ'''
+newnum2 = set(newnum)
+print(newnum2)
