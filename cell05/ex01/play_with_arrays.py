a2906672ca1d7b2b1412c58d015694a8edc91199
Phachthanasorn Peerawatkul <phapeera@z1t4c4.42bangkok.com>
2025-05-06 15:08:04 +0700

    ahhhhhh

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 num =  [2, 8, 9, 48, 8, 22, -12, 2]
-num2 = 0
-for num2 in num :
-    num2 = num + 2
+result = []
+for x in num:
+    '''append(data)'''
+    result.append (x + 2) 
 
 print(f"Original array: {num}")
-print(f"New array: {num2}")
+print(f"New array: {result}")
