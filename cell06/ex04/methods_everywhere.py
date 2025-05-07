be4ffa7229b5ac752e6e0921b3995b7257e9f3e0
Phachthanasorn Peerawatkul <phapeera@z2t10c3.42bangkok.com>
2025-05-07 12:25:24 +0700

    dwady

--- cell06/ex04/methods_everywhere.py
+++ cell06/ex04/methods_everywhere.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 import sys
 
-def shrink(s){
+def shrink(s):
     print(s[:8])
-}
 
-def enlarge(){
+def enlarge(s):
+    print(s + "Z" * (8 - len(s)))
 
-}
+args = sys.argv
 
-if len(sys.argv) <= 1:
+if len(args) == 1:
     print("none")
 else :
-    for text in 
+    for text in args:
+        if len(text) > 8:
+            shrink(text)
+        elif len(text) < 8 :
+            enlarge(text)
+        else :
+            print(text)
